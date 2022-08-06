b378c670ba95795876c9aecf52a463f2e719bb1e
Jake <jacob37857@gmail.com>
2022-08-05 23:26:22 -0400

    initial folder structure/barebone app

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Hello />
+        </div>
+    );
+};
+
+export default App;
