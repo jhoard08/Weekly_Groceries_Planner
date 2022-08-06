9359ef3e4e22f509e6ac395c8907104fef3d7472
Jake <jacob37857@gmail.com>
2022-08-06 00:17:45 -0400

    fixed vite.config.js to import tsx runtime to remove imports

--- src/app/components/Hello.tsx
+++ src/app/components/Hello.tsx
@@ -1,5 +1,4 @@
 import Container from './Container/Container';
-import React from 'react';
 
 const Hello: React.FC = () => {
     return (
--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: `http://localhost:${PORT}`,
+                changeOrigin: true
+            }
+        }
     },
-  },
-  build: {
-    outDir: 'dist/app',
-  },
+    build: {
+        outDir: 'dist/app'
+    }
 });
